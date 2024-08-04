8f65ac7f51d4a834a5e951edb3c64c293fee8952
nullpointer-excelsior <red.tiger.developer@proton.me>
2024-08-03 20:54:46 -0400

    register openai apikey ui done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
-'use client';
+'use client'
+import { useState } from "react";
+import TextInput from "../components/TextInput";
+import PrimaryButton from "../components/PrimaryButton";
 
-import { type CoreMessage } from 'ai';
-import { useState } from 'react';
 
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
+export default function Page() {
+  const [name, setName] = useState('')
+  const handleSubmit = (e: any) => {
 
-export default function Chat() {
-  const [messages, setMessages] = useState<CoreMessage[]>([]);
-  const [input, setInput] = useState('');
-  const [data, setData] = useState<any>();
+  }
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      <h1>Landing page</h1>
+    <div className="p-20 flex flex-col items-center">
+      <div className="flex flex-col items-center py-10 bg-black/50 w-2/3 shadow-md border border-white rounded-xl space-x-4">
+        <p className="text-3xl py-6 font-bold text-lime-700">Registro Modelo IA</p>
+        <div className=' text-sm'>
+          <p className="text-white text-xl p-4">Para usar Restobot deberás registrar una APIKEY de Openia . esta apikey NO se guarda en ninguna base de datossolo deforma local en el navegador de tu computador</p>
+          <div className="flex flex-col items-center gap-4 m-4">
+            <TextInput
+              className="bg-transparent text-white w-2/3"
+              placeholder='OpenAI API KEY'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <PrimaryButton className='w-56' disabled={name === ''} onClick={handleSubmit}>REGISTRAR APIKEY</PrimaryButton>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
