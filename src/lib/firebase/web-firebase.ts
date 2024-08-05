acd095a8f356e9f9a669ba1e8ea6ed447de59db5
nullpointer-excelsior <red.tiger.developer@proton.me>
2024-08-05 12:12:05 -0400

    integrating appcheck

--- src/lib/firebase/web-firebase.ts
+++ src/lib/firebase/web-firebase.ts
@@ -1,5 +1,6 @@
 import { getFirestore } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
@@ -10,6 +11,16 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_APP_ID,
     measurementId: process.env.NEXT_PUBLIC_MEASURENMENT_ID,
 };
+const environment =  process.env.ENVIRONMENT
 
 const app = initializeApp(firebaseConfig);
+
+if ( environment === 'prod') {
+    initializeAppCheck(app, {
+        provider: new ReCaptchaV3Provider('6LeiGiAqAAAAAAQeDs1Bi5exbUprimsJbva7-xvr'),
+        isTokenAutoRefreshEnabled: true
+    });
+}
+
 export const db = getFirestore(app)
+
